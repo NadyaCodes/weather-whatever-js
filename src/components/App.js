c640cefc0818a9c515272281e60ac1c2749f777a
NadyaCodes <nadyacorscadden@gmail.com>
2022-08-16 11:18:16 -0400

    add animated question marks and gradient banner

--- src/components/App.js
+++ src/components/App.js
@@ -43,7 +43,20 @@ function App() {
   return (
     <div className="site">
       <div className="pageTitle">
-        <h1>What is the Whether?</h1>
+        <span className="qleft"><span className="q1"><h1>?</h1></span>
+        <span className="q2"><h1>?</h1></span>
+        <span className="q3"><h1>?</h1></span>
+        <span className="q4"><h1>?</h1></span>
+        <span className="q5"><h1>?</h1></span>
+        </span>
+        <h1>What is the Weather</h1>
+        <span className="qright">
+        <span className="q5"><h1>?</h1></span>
+        <span className="q4"><h1>?</h1></span>
+        <span className="q3"><h1>?</h1></span>
+        <span className="q2"><h1>?</h1></span>
+        <span className="q1"><h1>?</h1></span>
+        </span>
       </div>
       <div className="container siteContent">
         <LocationSearch onSearch={addLocation} locations={locations} />
