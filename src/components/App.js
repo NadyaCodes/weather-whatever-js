90673aff6d491e07e42c96f51c896f09a34bf04b
NadyaCodes <nadyacorscadden@gmail.com>
2022-08-16 11:36:24 -0400

    change button colors

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "../App.css";
+import "../styles/App.scss";
 import LocationSearch from "./LocationSearch";
 import LocationTable from "./LocationTable";
 import WeatherDisplay from "./WeatherDisplay";
